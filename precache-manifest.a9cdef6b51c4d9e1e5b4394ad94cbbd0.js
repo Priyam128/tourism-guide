608ffd55ca5bead82353fa7aa345705e3f197ba3
Priyam Chakraborty <pc5933@srmist.edu.in>
2022-11-26 23:29:19 +0530

    Updates

---
+++ precache-manifest.a9cdef6b51c4d9e1e5b4394ad94cbbd0.js
@@ -0,0 +1,254 @@
+self.__precacheManifest = [
+  {
+    "revision": "e722e268ed39814b4c8d",
+    "url": "/tourism-guide/static/css/main.a34f910e.chunk.css"
+  },
+  {
+    "revision": "e722e268ed39814b4c8d",
+    "url": "/tourism-guide/static/js/main.e722e268.chunk.js"
+  },
+  {
+    "revision": "b3b558e90ccdb61f9dc2",
+    "url": "/tourism-guide/static/js/1.b3b558e9.chunk.js"
+  },
+  {
+    "revision": "6b6021131a67870cb5b8",
+    "url": "/tourism-guide/static/js/runtime~main.6b602113.js"
+  },
+  {
+    "revision": "1a72fbe95a497cc19609568e2464c954",
+    "url": "/tourism-guide/static/media/Bihar.1a72fbe9.jpg"
+  },
+  {
+    "revision": "0630bfc9119302a7fd0350f3a3805d14",
+    "url": "/tourism-guide/static/media/Delhi.0630bfc9.jpg"
+  },
+  {
+    "revision": "83827340c155143cdfa7e9a8a3266ba1",
+    "url": "/tourism-guide/static/media/WestBengal.83827340.webp"
+  },
+  {
+    "revision": "f27a64dbaa6633ef90ab7a66a6e8550d",
+    "url": "/tourism-guide/static/media/Maharashtra.f27a64db.jpeg"
+  },
+  {
+    "revision": "c250f70218c2e9830ec36f0d502f6c29",
+    "url": "/tourism-guide/static/media/TamilNadu.c250f702.jpg"
+  },
+  {
+    "revision": "25cf52d3d0ca4fc19a8cd4f5ddac31a5",
+    "url": "/tourism-guide/static/media/Goa.25cf52d3.jpg"
+  },
+  {
+    "revision": "c93aaa7edc160dd044afacf950bf5439",
+    "url": "/tourism-guide/static/media/Rajasthan.c93aaa7e.jpg"
+  },
+  {
+    "revision": "8d503bb2ceae033d2b8c89a28e584cdf",
+    "url": "/tourism-guide/static/media/UttarPradesh.8d503bb2.jpg"
+  },
+  {
+    "revision": "d1b90f1f54fafecab2cb4481013afce0",
+    "url": "/tourism-guide/static/media/Punjab.d1b90f1f.jpg"
+  },
+  {
+    "revision": "8ef385d47f4007bb4dd4356697171d80",
+    "url": "/tourism-guide/static/media/Kerala.8ef385d4.jpg"
+  },
+  {
+    "revision": "89c60dd7c551139b17908a1df50260e1",
+    "url": "/tourism-guide/static/media/Home.89c60dd7.jpg"
+  },
+  {
+    "revision": "99e17fb5833f4a3c6b1d6b34d8b7118f",
+    "url": "/tourism-guide/static/media/TajMahal1.99e17fb5.jpg"
+  },
+  {
+    "revision": "066fa32a40bf5a2ad19f02fb7f78458e",
+    "url": "/tourism-guide/static/media/TajMahal2.066fa32a.webp"
+  },
+  {
+    "revision": "da68640ab326349167a389b82f2791bc",
+    "url": "/tourism-guide/static/media/Varanasi1.da68640a.jpg"
+  },
+  {
+    "revision": "2856fb377399d9db290353f45ed0edc6",
+    "url": "/tourism-guide/static/media/Varanasi2.2856fb37.webp"
+  },
+  {
+    "revision": "f7fa3d7d809bbb156a3ecba8b62369a8",
+    "url": "/tourism-guide/static/media/About.f7fa3d7d.jpg"
+  },
+  {
+    "revision": "6a42dbf0f735e765d1d04e621f0b6dad",
+    "url": "/tourism-guide/static/media/States.6a42dbf0.jpg"
+  },
+  {
+    "revision": "345752988978f00c9b924707c6921490",
+    "url": "/tourism-guide/static/media/Contact.34575298.jpg"
+  },
+  {
+    "revision": "fe736ccd595377747e2e75b720e5eedc",
+    "url": "/tourism-guide/static/media/Bihar-Places.fe736ccd.jpg"
+  },
+  {
+    "revision": "c26126b3d26b1bc2c14280f8aada9803",
+    "url": "/tourism-guide/static/media/Bihar-Food.c26126b3.jpg"
+  },
+  {
+    "revision": "1c0266b2cd24d2b27949a75589fb87f0",
+    "url": "/tourism-guide/static/media/Bihar-Dress.1c0266b2.webp"
+  },
+  {
+    "revision": "2f9a45711680678d165a58424a1fef93",
+    "url": "/tourism-guide/static/media/Bihar-Art.2f9a4571.jpg"
+  },
+  {
+    "revision": "961b9baf3b5e86a2f6ad3f73aff6af77",
+    "url": "/tourism-guide/static/media/Delhi-Places.961b9baf.jpg"
+  },
+  {
+    "revision": "1d62997fedb8464dea3cf048a5609794",
+    "url": "/tourism-guide/static/media/Delhi-Food.1d62997f.avif"
+  },
+  {
+    "revision": "06bcc1e0bf032d96a4dbedfd581b3679",
+    "url": "/tourism-guide/static/media/Delhi-Dress.06bcc1e0.jpg"
+  },
+  {
+    "revision": "32be0d4955bcf37dfae1387ec1c14a08",
+    "url": "/tourism-guide/static/media/Delhi-Art.32be0d49.jpg"
+  },
+  {
+    "revision": "498e1326541df989ba9a2bcbcf501ba8",
+    "url": "/tourism-guide/static/media/Rajasthan-Places.498e1326.jpg"
+  },
+  {
+    "revision": "9a10391a68d0217ec6a05c13a67f2e13",
+    "url": "/tourism-guide/static/media/Rajasthan-Food.9a10391a.jpg"
+  },
+  {
+    "revision": "2e57eb0cfc58e2c52c34b2d644014430",
+    "url": "/tourism-guide/static/media/Rajasthan-Dress.2e57eb0c.jpg"
+  },
+  {
+    "revision": "683292de8c94c433e2c0c40eecdfde12",
+    "url": "/tourism-guide/static/media/Rajasthan-Art.683292de.jpg"
+  },
+  {
+    "revision": "7d893e24b872b0e154a5d859feec1c43",
+    "url": "/tourism-guide/static/media/Goa-Places.7d893e24.jpg"
+  },
+  {
+    "revision": "3a5cdaa068404de2c0fa7fd6388c46a4",
+    "url": "/tourism-guide/static/media/Goa-Food.3a5cdaa0.jpg"
+  },
+  {
+    "revision": "fc114781e02dee699d82fea3be908903",
+    "url": "/tourism-guide/static/media/Goa-Dress.fc114781.jpg"
+  },
+  {
+    "revision": "5e424ba21cf8364653c442af2c1e7235",
+    "url": "/tourism-guide/static/media/Goa-Art.5e424ba2.webp"
+  },
+  {
+    "revision": "1a6275157bef345f0bdf6dba3902c79c",
+    "url": "/tourism-guide/static/media/Maharashtra-Places.1a627515.jpg"
+  },
+  {
+    "revision": "cd08eac42fe48a31b57c009be5e7fb6d",
+    "url": "/tourism-guide/static/media/Maharashtra-Food.cd08eac4.jpg"
+  },
+  {
+    "revision": "8a04e720eeffefe8bfbdac1d0c3bd51d",
+    "url": "/tourism-guide/static/media/Maharashtra-Dress.8a04e720.webp"
+  },
+  {
+    "revision": "aa1c2900814960cc3ff3e8e168c436e1",
+    "url": "/tourism-guide/static/media/Maharashtra-Art.aa1c2900.jpg"
+  },
+  {
+    "revision": "f3e59352b427fbf8a8fed5864f559ba3",
+    "url": "/tourism-guide/static/media/TamilNadu-Places.f3e59352.jpg"
+  },
+  {
+    "revision": "40fae4ff4c9ca15796fb2f40aa75c216",
+    "url": "/tourism-guide/static/media/TamilNadu-Food.40fae4ff.jpg"
+  },
+  {
+    "revision": "e4372369ab97652804fb523b8bf2ec3f",
+    "url": "/tourism-guide/static/media/TamilNadu-Dress.e4372369.jpg"
+  },
+  {
+    "revision": "5025442da069bf2a2cdd2671e1ab104d",
+    "url": "/tourism-guide/static/media/TamilNadu-Art.5025442d.jpg"
+  },
+  {
+    "revision": "b87ab59792a9c32e738ced5b0ca59284",
+    "url": "/tourism-guide/static/media/Kerala-Places.b87ab597.jpg"
+  },
+  {
+    "revision": "b3f4dad4754c3cc1416e974dc66f6576",
+    "url": "/tourism-guide/static/media/Kerala-Food.b3f4dad4.webp"
+  },
+  {
+    "revision": "5e229eaf54456e72ff34563d21ec669e",
+    "url": "/tourism-guide/static/media/Kerala-Dress.5e229eaf.jpeg"
+  },
+  {
+    "revision": "41a29a857f3e36785276ce5efa0162f7",
+    "url": "/tourism-guide/static/media/Kerala-Art.41a29a85.jpg"
+  },
+  {
+    "revision": "b8c6d57144f9cab7b941419f1840d342",
+    "url": "/tourism-guide/static/media/Punjab-Places.b8c6d571.jpg"
+  },
+  {
+    "revision": "0fd3561a9803bcdd4bc6ba4c575f7b46",
+    "url": "/tourism-guide/static/media/Punjab-Food.0fd3561a.jpg"
+  },
+  {
+    "revision": "edfad7e5024ddb2e2cefb0e7d952d783",
+    "url": "/tourism-guide/static/media/Punjab-Dress.edfad7e5.jpg"
+  },
+  {
+    "revision": "ec3c09c2329b8c57da8e95e029e435c9",
+    "url": "/tourism-guide/static/media/Punjab-Art.ec3c09c2.jpg"
+  },
+  {
+    "revision": "5db19e6cd825560ca49ec4aa5bf5af4c",
+    "url": "/tourism-guide/static/media/UttarPradesh-Places.5db19e6c.jpg"
+  },
+  {
+    "revision": "2bf1edc131791fe4c5e7a17890f34832",
+    "url": "/tourism-guide/static/media/UttarPradesh-Food.2bf1edc1.jpg"
+  },
+  {
+    "revision": "c0789e6270c9231299ad4900882ea8de",
+    "url": "/tourism-guide/static/media/UttarPradesh-Dress.c0789e62.png"
+  },
+  {
+    "revision": "439a802abeb3335f16c2c66231866b53",
+    "url": "/tourism-guide/static/media/UttarPradesh-Art.439a802a.jpg"
+  },
+  {
+    "revision": "eec6d597780d0b7cc7b8aeedc1098487",
+    "url": "/tourism-guide/static/media/WestBengal-Places.eec6d597.jpg"
+  },
+  {
+    "revision": "18b5cd88a61e1e625a604755a07a9988",
+    "url": "/tourism-guide/static/media/WestBengal-Food.18b5cd88.jpg"
+  },
+  {
+    "revision": "5e6997db4f28ff187647dd70613d41ca",
+    "url": "/tourism-guide/static/media/WestBengal-Dress.5e6997db.png"
+  },
+  {
+    "revision": "d73a246ec6e4cb3ced8e77f66d3b3371",
+    "url": "/tourism-guide/static/media/WestBengal-Art.d73a246e.jpg"
+  },
+  {
+    "revision": "273ba23c56e97d765ba19afc4bb35785",
+    "url": "/tourism-guide/index.html"
+  }
+];
