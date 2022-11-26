0520345d1d779e2fb44278cafdc4ad0cfb8a9e14
Priyam Chakraborty <pc5933@srmist.edu.in>
2022-11-27 00:25:58 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e41c029c9bb5823a1003a42e1b23bc89.js"
+  "/tourism-guide/precache-manifest.76b8bde642f4d6be6e76bfbea78f0362.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/tourism-guide/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
