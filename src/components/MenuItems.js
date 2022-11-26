25b1c5ea11360fcf1690381515985ec5a3e3efe7
Priyam Chakraborty <pc5933@srmist.edu.in>
2022-11-27 00:51:50 +0530

    Update MenuItems.js

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -1,7 +1,7 @@
 export const MenuItems =[
     {
         title : "Home",
-        url : "/home",
+        url : "/tourism-guide",
         cName : "nav-links",
         icon : "fa-solid fa-house-user"
     } ,
